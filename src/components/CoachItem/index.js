16b0678b50af5343f92388fce40bce422a1943bc
eveins <maxmost512@gmail.com>
2020-02-23 10:37:30 +0200

    selected elements bugfix

--- src/components/CoachItem/index.js
+++ src/components/CoachItem/index.js
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import coachItemStyles from './item.module.css';
 
 class CoachItem extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      isSelected: false,
-      error: null,
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSelected: false,
+            error: null,
+        };
+    }
+
+    onClickHandler = () => {
+
+        this.setState({
+            isSelected: !this.state.isSelected
+        });
+
+        this.props.onSelectChange(this.props.coach);
     };
-  }
 
-  onClickHandler = () => {
-    this.setState({
-                    isSelected: !this.state.isSelected
-                  });
-    this.props.coach.isSelected = !this.props.coach.isSelected;
-    this.props.onSelectChange(this.props.coach);
-  };
+    render() {
+        const {profilePicture, firstName, lastName} = this.props.coach;
+        const index = this.props.level;
 
-  render () {
-    const { profilePicture, firstName, lastName } = this.props.coach;
-    const index = this.props.level;
+        return (
+            <div className={coachItemStyles.container}>
 
-    return (
-      <div className={coachItemStyles.container}>
+                <img src={profilePicture} alt={'coach'}
+                     className={coachItemStyles.coachPhoto}/>
 
-        <img src={profilePicture} alt={'coach'}
-             className={coachItemStyles.coachPhoto}/>
+                <div className={coachItemStyles.userInfo}>
+                    <div className={coachItemStyles.userInfoName}>{firstName + ' ' +
+                    lastName}</div>
+                    <div className={coachItemStyles.userLevel}>{`Level ${index}`}</div>
+                </div>
 
-        <div className={coachItemStyles.userInfo}>
-          <div className={coachItemStyles.userInfoName}>{firstName + ' ' +
-                                                         lastName}</div>
-          <div className={coachItemStyles.userLevel}>{`Level ${index}`}</div>
-        </div>
+                <div onClick={this.onClickHandler}
+                     className={this.state.isSelected
+                         ? coachItemStyles.isSelectedElement
+                         : coachItemStyles.selectElement}>
+                </div>
 
-        <div onClick={this.onClickHandler}
-             className={this.state.isSelected
-                        ? coachItemStyles.isSelectedElement
-                        : coachItemStyles.selectElement}>
-        </div>
-
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 
 }
 
--- src/components/CoachList/index.js
+++ src/components/CoachList/index.js
@@ -1,76 +1,84 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CoachItem from '../CoachItem';
 import coachListStyle from './list.module.css';
 
 class CoachList extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      selected: [],
-      coaches: [],
-      isFetching: false,
-      error: null,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: [],
+            coaches: [],
+            isFetching: false,
+            error: null,
+        };
+    }
 
-  loadData = () => {
-    const limit = 5;
+    loadData = () => {
+        const limit = 5;
 
-    this.setState({
-                    isFetching: true,
-                  });
-    fetch(
-      `http://192.168.0.113:3030/admin/users?limit=${limit}&offset=${this.state.coaches.length}`)
-      .then(res => res.json())
-      .then(data => {
         this.setState({
-                        coaches: [...this.state.coaches, ...data],
-                        isFetching: false,
-                      });
-      })
-      .catch((e) => {
-        this.setState({ error: e, isFetching: false });
-      });
-  };
+            isFetching: true,
+        });
+        fetch(
+            `http://localhost:3030/admin/users?limit=${limit}&offset=${this.state.coaches.length}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    coaches: [...this.state.coaches, ...data],
+                    isFetching: false,
+                });
+            })
+            .catch((e) => {
+                this.setState({error: e, isFetching: false});
+            });
+    };
 
-  componentDidMount () {
-    this.loadData();
-  }
+    componentDidMount() {
+        this.loadData();
+    }
 
-  handleSelect = (coach) => {
+    handleSelect = (coach) => {
+        const selectedArr = this.state.selected;
 
-    let toArr=[{...this.state.selected, coach}];
+        if (selectedArr.includes(coach)) {
 
-    this.setState({
-                    selected: toArr
-                  });
-  };
+            selectedArr.splice(selectedArr.indexOf(coach), 1);
+            this.setState({
+                selected: selectedArr
+            })
 
-  renderCoaches = () => {
-    const { coaches } = this.state;
+        } else {
+            this.setState({
+                selected: [...selectedArr, coach]
+            });
+        }
+    };
 
-    return coaches.map((item) => (
+    renderCoaches = () => {
+        const {coaches} = this.state;
 
-      <li key={item.id}
-          className={coachListStyle.container}>
-        <CoachItem onSelectChange={this.handleSelect}
-                   coach={{ ...item, isSelected: false }}
-                   level={item.id}/>
-      </li>
-    ));
-  };
+        return coaches.map((item) => (
 
-  render () {
-    let { selected } = this.state;
+            <li key={item.id}
+                className={coachListStyle.container}>
+                <CoachItem onSelectChange={this.handleSelect}
+                           coach={item}
+                           level={item.id}/>
+            </li>
+        ));
+    };
 
-    console.log(selected);
+    render() {
+        let {selected} = this.state;
 
-    return (<>
-        <div>selected</div>
-        <ul>{this.renderCoaches()}</ul>
-      </>
-    );
-  }
+        console.log(selected);
+
+        return (<>
+                <div>selected</div>
+                <ul>{this.renderCoaches()}</ul>
+            </>
+        );
+    }
 }
 
 export default CoachList;
